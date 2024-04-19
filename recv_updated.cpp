2f0b01dfd9ca1225a6c2eeb761e697db7c1a489d
Mridul-Nigam <151842671+Mridul-Nigam@users.noreply.github.com>
2024-04-19 10:39:46 +0530

    Update recv_updated.cpp

--- recv_updated.cpp
+++ recv_updated.cpp
@@ -37,38 +37,38 @@ struct Position {
 int sendAnswer = 0;
 int recvMsg = 1;
 char comma;
-Position receivedPosition; 
+Position receivedPosition;
 
 void on_open(server* s, websocketpp::connection_hdl hdl) {
-	std::cout << "librecvsocket opened" << std::endl;
-	std::thread([s, hdl]() {
-		while (sendAnswer!=1) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-			
-		}
-	std::cout << "libanswer in thread" << libanswer << std::endl;
-	s->send(hdl, libanswer, websocketpp::frame::opcode::text);
-		}).detach();
+    std::cout << "librecvsocket opened" << std::endl;
+    std::thread([s, hdl]() {
+        while (sendAnswer != 1) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-	
+        }
+        std::cout << "libanswer in thread" << libanswer << std::endl;
+        s->send(hdl, libanswer, websocketpp::frame::opcode::text);
+        }).detach();
+
+
 }
 
 void on_close(server* s, websocketpp::connection_hdl hdl) {
-	std::cout << "Connection closed" << std::endl;
+    std::cout << "Connection closed" << std::endl;
 }
 
 void on_message(server* s, websocketpp::connection_hdl hdl, server::message_ptr msg) {
-	if (recvMsg == 1) {
-		liboffer = msg->get_payload();
-		std::cout << "Received Liboffer: " << liboffer << std::endl;
-		recvMsg++;
-		peer2.setRemoteDescription(rtc::Description(liboffer));
-	}
-	else if (recvMsg == 2) {
-		libIceCandidate = msg->get_payload();
-		std::cout << "Received LibiceCandidate: " << libIceCandidate << std::endl;
-		peer2.addRemoteCandidate(rtc::Candidate(libIceCandidate));
-	}
+    if (recvMsg == 1) {
+        liboffer = msg->get_payload();
+        std::cout << "Received Liboffer: " << liboffer << std::endl;
+        recvMsg++;
+        peer2.setRemoteDescription(rtc::Description(liboffer));
+    }
+    else if (recvMsg == 2) {
+        libIceCandidate = msg->get_payload();
+        std::cout << "Received LibiceCandidate: " << libIceCandidate << std::endl;
+        peer2.addRemoteCandidate(rtc::Candidate(libIceCandidate));
+    }
 }
 
 int main(int  argc,
@@ -80,7 +80,7 @@ int main(int  argc,
     int    master, slave;
 
     // open and initialize 2 devices
-for (int dev = 0; dev < 1; dev++) {
+    for (int dev = 0; dev < 1; dev++) {
 
         // open device
         if (drdOpenID(dev) < 0) {
@@ -157,58 +157,58 @@ for (int dev = 0; dev < 1; dev++) {
     dhdSetForce(0.0, 0.0, 0.0, master);
     drdEnableFilter(true, slave);
 
-//--------------------------------------------------------------------------------------------------
-	peer2.onStateChange([](rtc::PeerConnection::State state) {
-		std::cout << "State: " << state << std::endl;
-		});
+    //--------------------------------------------------------------------------------------------------
+    peer2.onStateChange([](rtc::PeerConnection::State state) {
+        std::cout << "State: " << state << std::endl;
+        });
 
-	peer2.onLocalCandidate([](auto candidate) {
-		std::cout << "peer2 candidate: " << candidate << "\n";
-		});
+    peer2.onLocalCandidate([](auto candidate) {
+        std::cout << "peer2 candidate: " << candidate << "\n";
+        });
 
-	std::shared_ptr<rtc::DataChannel> channel2;
+    std::shared_ptr<rtc::DataChannel> channel2;
 
-	std::mutex printMutex;
+    std::mutex printMutex;
 
-	peer2.onDataChannel([&channel2, &printMutex](auto dc) {
-		std::cout << "peer 2 got data channel\n";
-		channel2 = dc;
-	dc->onMessage([&printMutex](auto message) {
-		std::unique_lock<std::mutex>(printMutex);
+    peer2.onDataChannel([&channel2, &printMutex](auto dc) {
+        std::cout << "peer 2 got data channel\n";
+        channel2 = dc;
+        dc->onMessage([&printMutex](auto message) {
+            std::unique_lock<std::mutex>(printMutex);
 
-	//std::cout << "message from peer1 : " << std::get<std::string>(message) << "\n";
-		
-		 std::stringstream ss(message);
-		 ss >> receivedPosition.i >> receivedPosition.x >> b >> receivedPosition.y >> c >> receivedPosition.z >> d;
+            //std::cout << "message from peer1 : " << std::get<std::string>(message) << "\n";
 
-  });
-	});
-	
-  peer2.onLocalDescription([](rtc::Description liboffer) {
-	libanswer = std::string(liboffer);
-	std::cout << "libanswer: " << libanswer << "\n";
-	sendAnswer = 1;
-		});
-  
-  // Register message handler
-		ws_server.set_message_handler(std::bind(&on_message, &ws_server, std::placeholders::_1, std::placeholders::_2));
-		ws_server.set_close_handler(std::bind(&on_close, &ws_server, std::placeholders::_1));
-		ws_server.set_open_handler(std::bind(&on_open, &ws_server, std::placeholders::_1));
-		 //Initialize the server
-		ws_server.init_asio();
-		ws_server.listen(9005);
-		ws_server.start_accept();
+            std::stringstream ss(std::get<std::string>(message));
+            ss >> receivedPosition.i >> receivedPosition.x >> receivedPosition.y >> receivedPosition.z;
 
-		// Run the server
-		ws_server.run();
-//---------------------------------------------------------------------------------------------------
-  
-    while (!done) {    
-       	std::cout<<receivedPosition.i<<std::endl;
+            });
+        });
+
+    peer2.onLocalDescription([](rtc::Description liboffer) {
+        libanswer = std::string(liboffer);
+        std::cout << "libanswer: " << libanswer << "\n";
+        sendAnswer = 1;
+        });
+
+    // Register message handler
+    ws_server.set_message_handler(std::bind(&on_message, &ws_server, std::placeholders::_1, std::placeholders::_2));
+    ws_server.set_close_handler(std::bind(&on_close, &ws_server, std::placeholders::_1));
+    ws_server.set_open_handler(std::bind(&on_open, &ws_server, std::placeholders::_1));
+    //Initialize the server
+    ws_server.init_asio();
+    ws_server.listen(9005);
+    ws_server.start_accept();
+
+    // Run the server
+    ws_server.run();
+    //---------------------------------------------------------------------------------------------------
+
+    while (!done) {
+        std::cout << receivedPosition.i << std::endl;
         drdTrackPos(receivedPosition.x, receivedPosition.y, receivedPosition.z);
     }
 
-// print stats and check for exit condition
+    // print stats and check for exit condition
     time = dhdGetTime();
 
     // report exit cause
